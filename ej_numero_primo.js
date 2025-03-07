8a13798354a7d08e960e70cd03da76662939027f
Ferran <ferran.docencia@gmail.com>
2025-03-07 11:38:14 +0100

    2025-03-07

---
+++ ej_numero_primo.js
@@ -0,0 +1,13 @@
+/*
+Dado un número:
+const numero = 12
+
+Indicar si es primo o no
+    -- Si lo es: "El numero X es primo"
+    -- Si no lo es : "El número X no es primo"
+
+Un número primo es un número entero que sólo es divisible por sí mismo o 1
+Por defecto ni 0, ni 1 se consideran primos
+El primer número primo es 2 
+
+*/
