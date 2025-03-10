a39498abae7df665fccaf2cea1c5e2c407c9b33b
Ferran <ferran.docencia@gmail.com>
2025-03-10 11:42:46 +0100

    2025-03-10

---
+++ anagramas.js
@@ -0,0 +1,12 @@
+// ANAGRAMAS
+// Dos palabras son anagramas si utilizan 
+// las mismas letras en orden diferente. Por ejemplo:
+// amor ramo 
+
+const palabra1 = "amor"
+const palabra2 = "ramo"
+const palabra3 = "patata"
+
+// Obtener esta salida
+// Las palabras "amor" y "ramo" son anagramas
+// Las palabras "amor" y "patata" no son anagramas
